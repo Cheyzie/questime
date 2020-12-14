c9c1b4b94cba00f513ac331995cf1fa608c369d7
Cheyzie <PyroDuckTail@gmail.com>
2020-12-15 01:26:30 +0200

    Create DB

--- questime/settings.py
+++ questime/settings.py
@@ -31,7 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'questie.apps.QuesieConfig',
+    'questie.apps.QuestieConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
