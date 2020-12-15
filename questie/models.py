a29b0c4c5709ba3f94c35aa4d9720d8534810ec7
Cheyzie <PyroDuckTail@gmail.com>
2020-12-15 15:05:20 +0200

    main

--- questie/models.py
+++ questie/models.py
@@ -5,6 +5,9 @@ class Quiz(models.Model):
     creation_date = models.DateTimeField('Дата создания',auto_now_add=True)
     quiz_name = models.CharField('Название теста',max_length=50)
 
+    def __str__(self):
+        return self.quiz_name
+
 class Question(models.Model):
     quiz = models.ForeignKey(Quiz,on_delete=models.CASCADE)
     wording = models.CharField('Формулировка вопроса',max_length=100)
@@ -12,11 +15,17 @@ class Question(models.Model):
     image = models.CharField('Ссылка на картинку', max_length=100)
     is_multiple_choice = models.BooleanField('Несколько ответов',default=False)
 
+    def __str__(self):
+        return self.wording
+
 
 class Choice(models.Model):
     question = models.ForeignKey(Question,on_delete=models.CASCADE)
     text = models.CharField('Текст ответа',max_length=100)
     is_correct = models.BooleanField('Верный ответ',default=False)
+
+    def __str__(self):
+        return self.text
 
 class Dude(models.Model):
     name = models.CharField('Имя',max_length=50)
